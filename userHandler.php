b65ebf922d7431281d2e295f84c4996ab38fcea5
Albert Widiatmoko <albert.widiatmoko@gmail.com>
2013-10-29 16:55:04 +0700

    Revert User Registration ID

--- userHandler.php
+++ userHandler.php
@@ -18,7 +18,7 @@ Class UserHandler {
             return $this->jsonHandler->createSimpleResponseMessage(1, "INVALID_DATA");
         }
         
-        $queryString = "SELECT id, name FROM users WHERE phone_number = '".$phoneNumber."'";
+        $queryString = "SELECT name FROM users WHERE phone_number = '".$phoneNumber."'";
         
         $this->database->query($queryString);
         
@@ -27,14 +27,12 @@ Class UserHandler {
                 $row = $this->database->result->fetch_array(MYSQLI_ASSOC);
                 
                 $messageArray = array("status" => "exists",
-                                      "id" => $row['id'],
                                       "name" => $row['name']);
                 
                 return $this->jsonHandler->encodeJson($messageArray);
             }
             else {
                 $messageArray = array("status" => "noexists",
-                                      "id" => "0",
                                       "name" => "");
                 
                 return $this->jsonHandler->encodeJson($messageArray);
@@ -50,27 +48,15 @@ Class UserHandler {
         if(!isset($phoneNumber) || !isset($userName) || !isset($gcmId) || !isset($publicKey)) {
             return $this->jsonHandler->createSimpleResponseMessage(1, "INVALID_DATA");
         }
-        $responseMessage = array();
         $queryString = "INSERT INTO users(phone_number, name, gcm_id, public_key) ". 
                        "VALUES('".$phoneNumber."', '".$userName."', '".$gcmId."', '".$publicKey."')";
             
         $this->database->query($queryString);
         if($this->database->result) {
-            $id = $this->database->insertId();
-            
-            $responseMessage['error'] = 0;
-            $responseMessage['message'] = "REG_SUCCESS";
-            $responseMessage['userid'] = $id;
-            
-            return $this->jsonHandler->encodeJson($responseMessage);
-            //return $this->jsonHandler->createSimpleResponseMessage(0, "REG_SUCCESS");
+            return $this->jsonHandler->createSimpleResponseMessage(0, "REG_SUCCESS");
         }
-        else {
-            $responseMessage['error'] = 1;
-            $responseMessage['message'] = "REG_ERROR";
-            
-            return $this->jsonHandler->encodeJson($responseMessage);
-            //return $this->jsonHandler->createSimpleResponseMessage(1, "REG_ERROR");
+        else {;
+            return $this->jsonHandler->createSimpleResponseMessage(1, "REG_ERROR");
         }
     }
     
@@ -136,9 +122,7 @@ Class UserHandler {
         
          // Set POST variables
         $url = 'https://android.googleapis.com/gcm/send';
-        $api_key = "AIzaSyAmxWvTVG-0OvAzV0m55Leip-EzaVq-Pts";
-        
-        
+        $api_key = "AIzaSyAmxWvTVG-0OvAzV0m55Leip-EzaVq-Pts"; 
     }
 }
 ?>
